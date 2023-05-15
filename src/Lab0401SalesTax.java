293519073bd9f14ffca16da9457d88ecdce3aa0a
JamesyLive <jamesjrbus@gmail.com>
2023-05-15 16:08:58 -0400

    Added task 1 code.

--- src/Lab0401SalesTax.java
+++ src/Lab0401SalesTax.java
@@ -4,6 +4,14 @@ public class Lab0401SalesTax
 {
     public static void main(String[] args)
     {
+        double SALES_TAX = 0.05;
+        double costOfItems = 12.25;
+        double taxCost = 0;
+        double totalCost = 0;
 
+        taxCost = SALES_TAX * costOfItems;
+        totalCost = costOfItems + taxCost;
+
+        System.out.println("Your total tax is " + taxCost + " and your total cost is " + totalCost + ".");
     }
 }
